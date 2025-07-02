f2ddfb937729d0703454556bb2faa0ec7fe6fb3f
Karl <karlbautista234@gmail.com>
2025-07-03 07:05:23 +0800

    put loading state and local storage

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,11 +1,29 @@
 
 import { useState } from "react";
 import "./styles/ToDoList.css"
+import { useEffect } from "react";
 const ToDoList = () => {
     
     const [list, setList] = useState([]);
     const [input, setInput] = useState("");
     const [editingIndex, setEditingIndex] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        
+        const listItem = localStorage.getItem("list");
+        if (listItem) setList(JSON.parse(listItem));
+        setLoading(false);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("list", JSON.stringify(list));
+    },[list]);
+
+    
+    if(loading){
+    return(<div className="loading">Loading.....</div>)
+   }
 
     const addToList = () => {
         if(input.trim() === "") return;
@@ -52,6 +70,7 @@ const ToDoList = () => {
        
 
    }
+
         return(
        <div className="todolist-main-container">
             <p id="title">To Do List (React)</p>
@@ -62,27 +81,18 @@ const ToDoList = () => {
                  <button id="input-btn" onClick={addToList}>Add</button>) :
                  (<button id="ok-btn" onClick={handleUpdateOk}>Ok</button>)}
             </div>   
-          
             <div className="list-container">
                 {list.map((l, i) => <div className="list-card" key={i} style={editingIndex === i ? {border: "5px solid rgb(25, 255, 102)"} : {}}>{l}
                     <div className="function-btns">
-
                          <button id="remove-btn" onClick={() => removeFromList(i)}>remove</button>
                         <button id="update-btn" onClick={() => updateItem(i)}>Update</button>
-                        <div key={i} onClick={() => moveItemUp(i)} id="move-item-up">☝️</div>
-                         <div key={i} onClick={() => moveItemDown(i)} id="move-item-down">👇</div>
-                          
-                    </div>
-                   
-                </div>)}
-                
+                        <div onClick={() => moveItemUp(i)} id="move-item-up">☝️</div>
+                         <div onClick={() => moveItemDown(i)} id="move-item-down">👇</div>    
+                    </div> 
+                </div>)}   
             </div>
             <div id="footer">{new Date().getFullYear() + " Karl Bautista"}</div>
-
-       </div>
-
-      
-    )
-}
+       </div>  
+    )}
 
 export default ToDoList;
